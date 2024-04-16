2616532ab20a679f5c67e21d1270a4e3357971d5
Camila <yourname@example.com>
2024-04-16 23:56:47 +0100

    Refactor popup functionality in game.js and add new main.js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,3 @@
-// Open the popup
-document.addEventListener("DOMContentLoaded", function() {
-  const openPopupButton = document.querySelector("#open-popup");
-  if (openPopupButton) {
-      openPopupButton.addEventListener("click", function() {
-          document.body.classList.add("active-popup");
-      });
-  }
-
-  const closePopupButton = document.querySelector(".popup .close-btn");
-  if (closePopupButton) {
-      closePopupButton.addEventListener("click", function() {
-          document.body.classList.remove("active-popup");
-      });
-  }
-});
-
 
 // Quiz questions logic
 function shuffleArray(array) {
---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Open the popup
+document.addEventListener("DOMContentLoaded", function() {
+    const openPopupButton = document.querySelector("#open-popup");
+    if (openPopupButton) {
+        openPopupButton.addEventListener("click", function() {
+            document.body.classList.add("active-popup");
+        });
+    }
+  
+    const closePopupButton = document.querySelector(".popup .close-btn");
+    if (closePopupButton) {
+        closePopupButton.addEventListener("click", function() {
+            document.body.classList.remove("active-popup");
+        });
+    }
+  });
